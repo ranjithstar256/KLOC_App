a31f066ef8a723d66b20aa68549efcee21d58bf9
Ranjith <ranjithstar256@gmail.com>
2024-01-02 16:37:54 +0530

    Initial commit

---
+++ app/src/main/java/kp/ran/klocapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package kp.ran.klocapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
+
+
+fun add ():String {
+    return ""
+}
+
+
+fun min( s : String ):String{
+    return "good"
+}
